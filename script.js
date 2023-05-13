384fdeb5c746436989edc0ae745deda28add69b1
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-05-13 19:26:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+	let s = str.replace(/ /g, "");
 
+	let j = s.length-1;
+	for(int i=0; i<s.length/2; i++, j--) {
+		if(s[i] != s[j])
+			return false;
+	}
+	return true;
 }
 module.exports = palindrome
